05c3ae2a56fd69e6de68efb2542d4e3afd0f25e7
SaChInD99 <sachindileepa41@gmail.com>
2024-01-05 02:19:45 +0530

    update

---
+++ tests/dropdown.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+const SEARCH_BAR :"//input[@id='searchField1']" = '//input[@id=\'searchField1\']';
+const ADVANCEDSEARCH :"//div[@class=\"advanced-search-link-wrapper\"]//a" = "//div[@class=\"advanced-search-link-wrapper\"]//a"
+const ANIMAL :"Animal Blood Groups and Biochemical Genetics" = "Animal Blood Groups and Biochemical Genetics";
+const FISH :"Fi" = "Fi";
+test('has title', async ({ page }) => {
+    await page.goto('https://onlinelibrary.wiley.com/action/showPublications');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Browse Publications - Wiley Online Library/), {setTimeout: 50000};
+
+    await page.evaluate(() => {
+        window.scrollBy(0, 50), {setTimeout: 150000};
+    });
+
+    await page.locator("#alphabetRange").selectOption({label:"A"}), {setTimeout: 150000}
+
+    await  page.waitForTimeout(1000)
+
+    await page.locator("#alphabetRange").selectOption({label:"B"});
+});
