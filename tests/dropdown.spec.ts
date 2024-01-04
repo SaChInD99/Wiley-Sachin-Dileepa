f8e318ee0a898e5200b49feb68b0c1f3febcb623
SaChInD99 <sachindileepa41@gmail.com>
2024-01-05 02:21:06 +0530

    update

--- tests/dropdown.spec.ts
+++ tests/dropdown.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 const SEARCH_BAR :"//input[@id='searchField1']" = '//input[@id=\'searchField1\']';
 const ADVANCEDSEARCH :"//div[@class=\"advanced-search-link-wrapper\"]//a" = "//div[@class=\"advanced-search-link-wrapper\"]//a"
 const ANIMAL :"Animal Blood Groups and Biochemical Genetics" = "Animal Blood Groups and Biochemical Genetics";
-const FISH :"Fi" = "Fi";
+
 test('has title', async ({ page }) => {
     await page.goto('https://onlinelibrary.wiley.com/action/showPublications');
 
