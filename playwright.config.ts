13eefc3684e663e7461fab95c32d522f9f038ef6
SaChInD99 <sachindileepa41@gmail.com>
2024-01-05 02:14:54 +0530

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+      ['html', {open: true}]
+      ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -38,18 +40,23 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'],
+      launchOptions:{
+        slowMo: 1000,
+      },
+      },
+
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    //{
+     // name: 'firefox',
+    //  use: { ...devices['Desktop Firefox'] },
+  //  },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+   // {
+  //    name: 'webkit',
+  //    use: { ...devices['Desktop Safari'] },
+  //  },
 
     /* Test against mobile viewports. */
     // {
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 const SEARCH_BAR :"//input[@id='searchField1']" = '//input[@id=\'searchField1\']';
+const ADVANCEDSEARCH :"//div[@class=\"advanced-search-link-wrapper\"]//a" = "//div[@class=\"advanced-search-link-wrapper\"]//a"
 const ANIMAL :"Animal Blood Groups and Biochemical Genetics" = "Animal Blood Groups and Biochemical Genetics";
-
+const FISH :"Fi" = "Fi";
 test('has title', async ({ page }) => {
   await page.goto('https://onlinelibrary.wiley.com/');
 
@@ -11,7 +12,17 @@ test('has title', async ({ page }) => {
 
   await page.locator(SEARCH_BAR).fill(ANIMAL), {setTimeout: 1000000};
 
-  await page.keyboard.press('Enter'), {setTimeout: 100000};
+  await page.locator(SEARCH_BAR).clear();
+
+  await page.locator(SEARCH_BAR).fill(FISH), {setTimeout: 1000000};
+
+  await expect(page.getByRole('heading', { name: 'Resources' })).toBeVisible();
+
+  await page.locator(ADVANCEDSEARCH).click();
+
+  await page.close();
+
+
 });
 
 test('get started link', async ({ page }) => {
